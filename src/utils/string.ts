a500b9e554e9065e53cdcde04096d902300e8280
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-03-16 02:31:05 +0900

    feat: add layout for blog detail

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,15 @@
+import { EmojiEntity, parse } from "twemoji-parser";
+
 export const capitalize = (s: string): string => {
   return s.charAt(0).toUpperCase() + s.slice(1, s.length);
+};
+
+export const convertEmojiStrToTwiImage = (s: string): EmojiEntity => {
+  const entities = parse(s);
+
+  if (entities.length !== 1) {
+    throw Error("1 emoji must be set");
+  }
+  const emojiEntity = entities[0];
+  return emojiEntity;
 };
