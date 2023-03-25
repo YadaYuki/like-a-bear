a70077f2ebc5f5dea3ca8dc427ba12585d9642a7
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-03-25 22:09:02 +0900

    fixed path

--- src/consts/path.ts
+++ src/consts/path.ts
@@ -1,9 +1,5 @@
-import { dirname, join } from "node:path";
+import { join } from "node:path";
 
-export const PROJECT_ROOT_DIR = join(
-  dirname(new URL(import.meta.url).pathname),
-  "..",
-  ".."
-);
+export const PROJECT_ROOT_DIR = process.cwd();
 export const SRC_DIR = join(PROJECT_ROOT_DIR, "src");
 export const FONT_DIR = join(SRC_DIR, "assets", "fonts");
