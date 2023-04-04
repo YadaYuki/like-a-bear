32dacbeaff02e0e1ce3d8f27b38940d3d6cb94e9
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-04-05 01:58:37 +0900

    Add form url

--- src/consts/url.ts
+++ src/consts/url.ts
@@ -1,5 +1,5 @@
 export const SITE_URL = "https://like-a-bear.pages.dev/"; // TODO: fix me!
 export const PORTFOLIO_URL = "https://www.yyada.jp/";
-export const CONTACT_URL = "GOOGLE_FORM_URL_HERE";
+export const CONTACT_URL = "https://forms.gle/dfo1VHBayiXtGeht7";
 export const TWITTER_URL = "https://twitter.com/YADAYUKI2";
 export const GITHUB_URL = "https://github.com/YadaYuki";
